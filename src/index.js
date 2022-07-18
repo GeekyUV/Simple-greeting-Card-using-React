6eeeeb7b3521f02aa12f7d4359a6b0ee50807b5c
yuvi <harryyuvi9@gmail.com>
2022-07-18 16:34:27 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let curDate = new Date();
+curDate = curDate.getHours();
+let greeting = "";
+const cssstyle = {};
+
+if (curDate >= 1 && curDate < 12) {
+  greeting = "Good Morning";
+  cssstyle.color = "green";
+} else if (curDate >= 12 && curDate < 15) {
+  greeting = "Good AfterNoon";
+  cssstyle.color = "yellow";
+} else if (curDate >= 15 && curDate < 19) {
+  greeting = "Good Evening";
+  cssstyle.color = "blue";
+} else {
+  greeting = "Good Night";
+  cssstyle.color = "pink";
+}
+
+ReactDOM.render(
+  <>
+    <div>
+      <h1>
+        Greeting yuvi <span style={cssstyle}> {greeting} </span>
+      </h1>
+    </div>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
